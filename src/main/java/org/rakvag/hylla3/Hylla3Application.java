ed0da06e63e200107abab37657c80abec52c6d04
Andre Rakvåg <ara@decisive.no>
2018-09-13 20:40:51 +0200

    Første commit - maven/spring boot lagt til.

---
+++ src/main/java/org/rakvag/hylla3/Hylla3Application.java
@@ -0,0 +1,12 @@
+package org.rakvag.hylla3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hylla3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hylla3Application.class, args);
+	}
+}
